4d013b9697577ed27f41100a34d1d607bf4d9420
kevinptx <kevin.peery@gmail.com>
2017-12-11 15:37:39 -0600

    rendering functions written

--- index.js
+++ index.js
@@ -6,7 +6,6 @@
 
 
 const STORE = {
-  view: ,
   currentQuestion: ,
   currentView: ,
   currentScore: ,
@@ -48,9 +47,23 @@ const QUESTIONS = [
 
 /********************   DOM MANIPULATION FUNCTIONS   *******************/
 
-function renderQuestionList() {
-  console.log('`renderShoppingList` ran');
-  
+//top level renders everything on page. The master function decides which of the sub functions to call. It looks at which of your views to render...maybe question page, maybe final results page, etc...
+
+//the template is basically the JS rendering. Job of rendering is covered by the renderer and the generator. You can do them as one function or as 2, as you please.
+
+function renderQuestionText() {
+  console.log('`renderQuestion` ran');
+//what to put into renderQuestion to render 1 entire question. Display question here somehow. We do this from HTML.
+  //insert that HTML into the DOM
+  $('.js-answers').html(questionItemString);
+  $('.js-answers').find('input').focus();
+}
+
+//second level render is to render the individual sections.
+
+function renderAnswerList(question) {
+  console.log('Generating the answer list');
+
 }
 
 
@@ -62,10 +75,13 @@ function renderQuestionList() {
 function handleClickStart() {
   $('.js-click-start').on('submit', event => {
     event.preventDefault();
-    STORE.view = question;
+    //how do we render the first question page when we hit the start button? There's 2 separate parts to this. part 1 is how render function decides how to render first page? part 2 is how does start button do that to the STORE? How does renderer decide to render first question.
+
+    //with whatever array you're looking at, the first one is simply the variable zero. It's the index into QUESTIONS, not store. currentQuestion means index into QUESTIONS array. the currentQuestion STORE value represents which question you're on.
+    STORE.currentQuestion = 0;
 
     console.log('`handleClickStart` ran');
-    
+
   });
 }
 
