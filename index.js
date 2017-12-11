ada4d6b4ffcfff89a6373bc8a1d20ef7b64c565e
kevinptx <kevin.peery@gmail.com>
2017-12-11 11:33:13 -0600

    HTML started

---
+++ index.js
@@ -0,0 +1,4 @@
+{
+  question: "what's my name?"
+  //can make array a bunch of strings. have a correct answer property and then an incorrect answers array. or you could have an answers array and another that has the index of which is the correct answer. what do you need to put in DOM (radio values) to reference back to your data? could make answers string the unique value. using index value of the array may be safer. 
+}
