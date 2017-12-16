b09cb291cb915787404539c8366db304db94660d
Dan Rosen <drosen84@gmail.com>
2017-12-15 21:20:03 -0600

    changes to render function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const STORE = {
-    currentQuestion: null,
+    currentQuestion: -1,
     currentView: 'intro',
     currentScore: 0,
     currentCorrect: 0
@@ -9,77 +9,87 @@ const STORE = {
 
 const QUESTIONS = [
     {
-        qNum: '1 of 5',
+        qNum: 'Question 1 of 5',
         text: 'What do you get when you mix salt and water?',
         options: ['Lemonade', 'Taco surprise', 'Salt water', 'Glue'],
         correctAnswerIndex: 'Salt water'
     },
     {
-        qNum: '2 of 5',
+        qNum: 'Question 2 of 5',
         text: 'How are spiders made?',
         options: ['Spider eggs', 'Mixing ants and caterpillars', 'Stepping on M&Ms', 'Asking nicely'],
         correctAnswerIndex: 'Spider eggs'
     },
     {
-        qNum: '3 of 5',      
+        qNum: 'Question 3 of 5',      
         text: 'What is my middle name?',
         options: ['Alexander', 'Lewis', 'No clue', 'Richard'],
         correctAnswerIndex: 'No clue'
     },
     {
-        qNum: '4 of 5',
+        qNum: 'Question 4 of 5',
         text: 'What happens during a solar eclipse?',
         options: ['Water vapor from the ocean extinguishes the sun', 'The moon blocks our view of the sun', 'There is no such thing as a solar eclipse', 'The moon gets jealous and does something about it'],
         correctAnswerIndex: 'The moon blocks our view of the sun'
     },
     {
-        qNum: '5 of 5',
+        qNum: 'Question 5 of 5',
         text: 'Where do fish live?',
         options: ['The sky', 'In water', 'In volcanoes', 'In a tree',],
         correctAnswerIndex: 'In water'
     }
 ];
+
+
 //Render function:
 const render = function() {
+
   
+
 };
 
 function renderPage() {
+    $('.answer-incorrect').hide();
+    $('.answer-correct').hide();
+    $('.results').hide();
+    $('.quiz').hide();
+    $('.intro').hide();
+
     if (STORE.currentView === 'intro'){
         $('.intro').show();
-        $('.quiz').hide();
-        $('.results').hide();
-        $('.answer-incorrect').hide();
-        $('.answer-correct').hide();
+        // $('.quiz').hide();
+        // $('.results').hide();
+        // $('.answer-incorrect').hide();
+        // $('.answer-correct').hide();
     }
     else if (STORE.currentView === 'quiz'){
         $('.quiz').show();
-        $('.intro').hide();
-        $('.results').hide();
-        $('.answer-incorrect').hide();
-        $('.answer-correct').hide();
+        // $('.intro').hide();
+        // $('.results').hide();
+        // $('.answer-incorrect').hide();
+        // $('.answer-correct').hide();
         renderQuestionText();
     }
     else if (STORE.currentView === 'results'){
         $('.results').show();
-        $('.intro').hide();
-        $('.quiz').hide();
-        $('.answer-incorrect').hide();
-        $('.answer-correct').hide();
+        // $('.intro').hide();
+        // $('.quiz').hide();
+        // $('.answer-incorrect').hide();
+        // $('.answer-correct').hide();
     }
     else if (STORE.currentView === 'answer-incorrect'){
         $('.quiz').show();
         $('.answer-incorrect').show();
-        $('.intro').hide();
-        $('.results').hide();
-        $('.answer-correct').hide();
+        // $('.intro').hide();
+        // $('.results').hide();
+        // $('.answer-correct').hide();
     }
     else if (STORE.currentView === 'answer-correct'){
         $('.quiz').show();
         $('.answer-correct').show();
-        $('.intro').hide();
-        $('.results').hide();
-        $('.answer-incorrect').hide();
+        // $('.intro').hide();
+        // $('.results').hide();
+        // $('.answer-incorrect').hide();
         renderAnswerCorrect();
     }
 }
@@ -99,7 +109,7 @@ function renderQuestionText() {
     console.log('`renderQuestion` ran');
     $('.quiz').html(`
   <form id="js-quiz-app-form">
-  <h2>"${STORE.currentQuestion} of 5"</h2>
+  <h2>"${QUESTIONS[STORE.currentQuestion].qNum}"</h2>
   <div class="js-answers">
     <input type="radio" id="choice1" name="answer" value="0"></input>
     <label for="choice1">${QUESTIONS[STORE.currentQuestion].options[0]}</label><br>
@@ -116,7 +126,7 @@ function renderQuestionText() {
 }
 
 function renderAnswerCorrect() {
-    console.log('`renderAnswerCorrect` ran');
+    console.log('renderAnswerCorrect ran');
     $('.answer-correct').html(`
     <h3>That's correct!</h3>
     <button type="submit" class="js-click-next">Next question</button>
@@ -124,11 +134,14 @@ function renderAnswerCorrect() {
  `);
 }
 
-// function renderQuestionCorrect() {
-//   console.log('`renderQuestionCorrect` ran');
+function renderAnswerIncorrect() {
+    console.log('renderAnswerIncorrect ran');
+    $('f'.html)(`
+  <h3>That's Incorrect!</h3>
+  <button type="submit" class="js-click-next">Next question</button>
+  <p class="num-correct">1/5 correct</p>`);
+}
 
-// function renderQuestionIncorrect() {
-//     console.log('`renderQuestionIncorrect` ran');
 
 // $('.js-answers').find('input').focus();
 
@@ -166,7 +179,7 @@ function handleQuestionSubmit() {
 function handleNextQuestion() {
     $('.answer-correct').on('submit', '.js-click-next', event => {
         event.preventDefault();
-        STORE.currentQuestion = ++STORE.currentQuestion;
+        STORE.currentQuestion++;
         STORE.currentView = 'quiz';
         console.log(STORE.currentQuestion);
         console.log('`handleNextQuestion` ran');
